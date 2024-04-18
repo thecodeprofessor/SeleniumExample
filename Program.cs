bbad049395b61c37c066a2a6a3322f8a4a058636
thecodeprofessor <nathan@abourbih.com>
2024-04-18 16:32:26 -0400

    Created selenium automation code to test case id 1.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,55 @@
-﻿namespace SeleniumExample
+﻿using OpenQA.Selenium.Edge;
+using OpenQA.Selenium;
+using System.Reflection.Metadata;
+
+namespace SeleniumExample
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Test_1_MyCambrianLogin_ValidUsername_NoPassword();
+        }
+
+        static void Test_1_MyCambrianLogin_ValidUsername_NoPassword()
+        {
+            var driver = new EdgeDriver();
+            try
+            {
+                driver.Url = "https://mycambrian.cambriancollege.ca/";
+
+                Console.Clear();
+
+                var loginForm = driver.FindElement(By.Id("fm1"));
+
+
+                var usernameTextBox = driver.FindElement(By.Id("username"));
+                usernameTextBox.SendKeys("A0011223");
+
+                var passwordTextBox = driver.FindElement(By.Id("password"));
+                passwordTextBox.Clear();
+
+                loginForm.Submit();
+
+                Thread.Sleep(2000);
+                var errorPanel = driver.FindElement(By.ClassName("errors"));
+
+                var errorMessageExpected = "Password is a required field.";
+                var errorMessageShown = errorPanel.Text;
+
+                if (errorMessageShown == errorMessageExpected)
+                {
+                    Console.WriteLine("Test MyCambrianLogin_ValidUsername_NoPassword: PASS");
+                }
+                else
+                {
+                    Console.WriteLine("Test MyCambrianLogin_ValidUsername_NoPassword: FAIL");
+                }
+            }
+            finally
+            {
+                driver.Quit();
+            }
         }
     }
 }
